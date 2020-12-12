bc8cd98634647b30efe52d30fe932e060868ce00
ukeyshima <ukeyshima@gmail.com>
2020-12-12 16:04:40 +0900

    first commit

---
+++ Assets/Scripts/GazeCursor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Assertions;
+using ViveSR.anipal.Eye;
+
+public class eyePositioning : MonoBehaviour
+{
+    public int LengthOfRay = 25;
+    [SerializeField] private LineRenderer GazeRayRenderer;
+    [SerializeField] private GameObject camera;
+    private static EyeData eyeData = new EyeData();
+    private bool eye_callback_registered = false;
+    private void Start()
+    {
+        if (!SRanipal_Eye_Framework.Instance.EnableEye)
+        {
+            enabled = false;
+            return;
+        }
+        Assert.IsNotNull(GazeRayRenderer);
+    }
+
+    private void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+            SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+
+        Vector3 GazeOriginCombinedLocal, GazeDirectionCombinedLocal;
+
+        if (eye_callback_registered)
+        {
+            if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else return;
+        }
+        else
+        {
+            if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else return;
+        }
+
+        Vector3 GazeDirectionCombined = camera.transform.TransformDirection(GazeDirectionCombinedLocal);
+        Vector3 Origin = camera.transform.position - camera.transform.up * 0.05f;
+
+        GazeRayRenderer.SetPosition(0, Origin);
+        GazeRayRenderer.SetPosition(1, camera.transform.position + GazeDirectionCombined * LengthOfRay);
+
+        RaycastHit hit;
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        if (Physics.Raycast(Origin,GazeDirectionCombined, out hit, LengthOfRay,layerMask))
+        {
+            Vector3 hitPoint = hit.point;
+            hitPoint.z -= 0.001f;
+            transform.position = hitPoint;
+        }
+    }
+    private void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+    private static void EyeCallback(ref EyeData eye_data)
+    {
+        eyeData = eye_data;
+    }
+}
---
+++ Assets/Scripts/GazeRay.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class GazeRay : MonoBehaviour
+{
+    public int LengthOfRay = 25;
+    [SerializeField] private LineRenderer GazeRayRenderer;
+    [SerializeField] private GameObject Camera;
+    private static EyeData eyeData = new EyeData();
+    private bool eye_callback_registered = false;
+    private void Start()
+    {
+        if (!SRanipal_Eye_Framework.Instance.EnableEye)
+        {
+            enabled = false;
+            return;
+        }
+        Assert.IsNotNull(GazeRayRenderer);
+    }
+
+    private void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+            SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+
+        Vector3 GazeOriginCombinedLocal, GazeDirectionCombinedLocal;
+
+        if (eye_callback_registered)
+        {
+            if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal, eyeData)) { }
+            else return;
+        }
+        else
+        {
+            if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else if (SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal)) { }
+            else return;
+        }
+
+        Vector3 GazeDirectionCombined = Camera.transform.TransformDirection(GazeDirectionCombinedLocal);
+        GazeRayRenderer.SetPosition(0, Camera.transform.position - Camera.transform.up * 0.05f);
+        GazeRayRenderer.SetPosition(1, Camera.transform.position + GazeDirectionCombined * LengthOfRay);
+    }
+    private void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+    private static void EyeCallback(ref EyeData eye_data)
+    {
+        eyeData = eye_data;
+    }
+}
